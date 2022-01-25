323523c96b3a0d23ee92ebad5234d2a79e1133bc
Fernando <illoqpaza2@gmail.com>
2022-01-25 09:25:40 +0100

    refactorizado componente App

---
+++ src/containers/App/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p>Prueba técnica de LaLiga</p>
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import store from "store";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./App";
+import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
