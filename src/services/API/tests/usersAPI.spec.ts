3c45e4f595cdbfbb40b0bdd37c72887d64cf3463
Fernando <illoqpaza2@gmail.com>
2022-01-25 18:41:45 +0100

    añadida nuevo test unitario. Descripción cambiada a español.

--- src/services/API/tests/usersAPI.spec.ts
+++ src/services/API/tests/usersAPI.spec.ts
@@ -1,6 +1,6 @@
 import { usersAPI } from "../usersAPI";
 
-test("user login get the token", async () => {
+test("login obtiene el token del usuario cuando es correcto.", async () => {
   const userMock = { username: "eve.holt@reqres.in", password: "cityslicka" };
   const userToken = await usersAPI.login(userMock);
   expect(userToken).toEqual(
@@ -8,8 +8,16 @@ test("user login get the token", async () => {
   );
 });
 
-test("user login get error if user empty", async () => {
+test("si el usuario está vacio devuelve un error", async () => {
   const userMock = { username: "", password: "" };
+  const userToken = await usersAPI.login(userMock);
+  expect(userToken).toEqual(
+    expect.objectContaining({ error: expect.any(String) })
+  );
+});
+
+test("si el usuario no existe devuelve un error", async () => {
+  const userMock = { username: "test", password: "nopuedeexistir" };
   const userToken = await usersAPI.login(userMock);
   expect(userToken).toEqual(
     expect.objectContaining({ error: expect.any(String) })
