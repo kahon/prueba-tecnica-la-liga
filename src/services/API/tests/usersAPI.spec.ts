c3d3630a45f583b45e7ada28ce1da5ca9d59386b
Fernando <illoqpaza2@gmail.com>
2022-01-25 18:45:40 +0100

    añadido describe en el test del usersAPI

--- src/services/API/tests/usersAPI.spec.ts
+++ src/services/API/tests/usersAPI.spec.ts
@@ -1,25 +1,27 @@
 import { usersAPI } from "../usersAPI";
 
-test("login obtiene el token del usuario cuando es correcto.", async () => {
-  const userMock = { username: "eve.holt@reqres.in", password: "cityslicka" };
-  const userToken = await usersAPI.login(userMock);
-  expect(userToken).toEqual(
-    expect.objectContaining({ token: expect.any(String) })
-  );
-});
+describe("test de userApi", () => {
+  test("login obtiene el token del usuario cuando es correcto.", async () => {
+    const userMock = { username: "eve.holt@reqres.in", password: "cityslicka" };
+    const userToken = await usersAPI.login(userMock);
+    expect(userToken).toEqual(
+      expect.objectContaining({ token: expect.any(String) })
+    );
+  });
 
-test("si el usuario está vacio devuelve un error", async () => {
-  const userMock = { username: "", password: "" };
-  const userToken = await usersAPI.login(userMock);
-  expect(userToken).toEqual(
-    expect.objectContaining({ error: expect.any(String) })
-  );
-});
+  test("si el usuario está vacio devuelve un error", async () => {
+    const userMock = { username: "", password: "" };
+    const userToken = await usersAPI.login(userMock);
+    expect(userToken).toEqual(
+      expect.objectContaining({ error: expect.any(String) })
+    );
+  });
 
-test("si el usuario no existe devuelve un error", async () => {
-  const userMock = { username: "test", password: "nopuedeexistir" };
-  const userToken = await usersAPI.login(userMock);
-  expect(userToken).toEqual(
-    expect.objectContaining({ error: expect.any(String) })
-  );
+  test("si el usuario no existe devuelve un error", async () => {
+    const userMock = { username: "test", password: "nopuedeexistir" };
+    const userToken = await usersAPI.login(userMock);
+    expect(userToken).toEqual(
+      expect.objectContaining({ error: expect.any(String) })
+    );
+  });
 });
