4e7f06e16b223cbd57444b1f627e4b627bdf3cdc
Fernando <illoqpaza2@gmail.com>
2022-01-27 00:28:05 +0100

    limpiado el AppRouter de unuseds

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,34 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Button from "components/Button";
 import LoginPage from "containers/LoginPage";
 import NotFoundPage from "containers/NotFoundPage";
-import { ACTIONS } from "store/actions";
 import { Header } from "components/Header";
-import useAuth from "hooks/useAuth";
 import UserListPage from "containers/UserListPage";
 import PrivateRoute from "./PrivateRoute";
 
-export const AppRouter = ({ store }) => {
-  const auth = useAuth();
-
+export const AppRouter = () => {
   return (
     <Router>
-      {/* <Button
-        onClick={() => {
-          store.dispatch({
-            type: ACTIONS.API.LOGIN_REQUEST,
-            payload: {
-              user: {
-                username: "eve.holt@reqres.in",
-                password: "cityslicka",
-              },
-            },
-          });
-        }}
-      >
-        Test Redux
-      </Button> */}
       <Header />
       <Switch>
         <Route exact path="/" component={LoginPage} />
