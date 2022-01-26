7dc61e2c909732b0cfa5647ab1b1eb702dd869e1
Fernando <illoqpaza2@gmail.com>
2022-01-27 00:29:27 +0100

    actualizados los test de LoginForm, solucionando fallo.

--- src/components/LoginForm/tests/index.test.js
+++ src/components/LoginForm/tests/index.test.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import LoginForm from "..";
+import { Provider } from "react-redux";
+import store from "store";
 
 describe("Test LoginForm", () => {
   test("debe renderizar el form", () => {
-    const { container } = render(<LoginForm />);
+    const { container } = render(
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
     expect(Boolean(container.querySelector("form"))).toBe(true);
   });
   test("debe renderizar al menos un input", () => {
-    const { container } = render(<LoginForm />);
+    const { container } = render(
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
     expect(Boolean(container.querySelector("input"))).toBe(true);
   });
   test("debe renderizar un button", () => {
-    const { container } = render(<LoginForm />);
+    const { container } = render(
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
     expect(Boolean(container.querySelector("button"))).toBe(true);
   });
 });
