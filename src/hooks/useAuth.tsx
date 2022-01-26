c1c5d93b4756c1e1115c66807df191b4111239c4
Fernando <illoqpaza2@gmail.com>
2022-01-27 00:08:21 +0100

    he añadido la subscripción para actualizar el hook de useAuth.

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import { useStore } from "react-redux";
+import { Store } from "redux";
 
 export default function useAuth() {
   const store = useStore();
+  const [token, setToken] = useState(selectToken(store));
 
+  function selectToken(store: Store): string {
+    return store.getState()["apiReducer"]["token"];
+  }
   const isAuth = (): Boolean => {
-    return Boolean(store.getState()["apiReducer"]["token"]);
+    return Boolean(token);
   };
 
-  return { isAuth };
+  store.subscribe(() => {
+    setToken(selectToken(store));
+  });
+
+  return { isAuth, token };
 }
