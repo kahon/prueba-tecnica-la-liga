40114c22931aca4d54be2c0b839e818059fcfaa0
Fernando <illoqpaza2@gmail.com>
2022-01-26 16:01:55 +0100

    añadido comentarios.

--- src/services/API/usersAPI.ts
+++ src/services/API/usersAPI.ts
@@ -1,15 +1,29 @@
 const URLS = { LOGIN: "https://reqres.in/api/login" };
 
+/**
+ * Tipo de entrada de usuario en el login
+ */
 interface UserLogin {
   username: string;
   password: string;
 }
+/**
+ * Tipo de resultado devuelto por la petición de login
+ */
 interface LoginResult {
   token?: string;
   error?: string;
 }
 
+/**
+ * Funciones de acceso a la API de usuarios
+ */
 const usersAPI = {
+  /**
+   * Función para hacer el login de usuario
+   * @param user usuario que se loguea
+   * @returns devuelve el token o el error
+   */
   login: async (user: UserLogin): Promise<LoginResult> => {
     const response = await fetch(URLS.LOGIN, {
       method: "POST",
