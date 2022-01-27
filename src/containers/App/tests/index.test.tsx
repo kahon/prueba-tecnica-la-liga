4116763e1c29a15a0a74142e56cba17daf5a3559
Fernando <illoqpaza2@gmail.com>
2022-01-27 19:45:09 +0100

    prueba unitaria, faltaba store Provider  FIXED

--- src/containers/App/tests/index.test.tsx
+++ src/containers/App/tests/index.test.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "../../App";
-
+import store from "store";
+import { Provider } from "react-redux";
 test("renders learn react link", () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const linkElement = getByText(/Prueba técnica de LaLiga/i);
   expect(linkElement).toBeInTheDocument();
 });
