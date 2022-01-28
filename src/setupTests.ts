fa284463858ee87269d346245ff63485efbd8ced
Fernando <illoqpaza2@gmail.com>
2022-01-28 13:49:53 +0100

    scrollTo error en el test de App, FIXED

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+// para que los test detecten esta función como una función que existe
+window.scrollTo = jest.fn();
