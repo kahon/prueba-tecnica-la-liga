962a1afa7548924930bba45fc7d7f81643f0e68e
Fernando <illoqpaza2@gmail.com>
2022-01-28 08:01:54 +0100

    comentario añadido

--- src/hooks/useLoading.tsx
+++ src/hooks/useLoading.tsx
@@ -1,7 +1,11 @@
 import { RootStateOrAny, useSelector } from "react-redux";
 
+/**
+ * Maneja el estado de loading del store de forma reactiva.
+ * @returns { isLoading: Boolean: indica si el estado de la aplicación está cargando. }
+ */
 const useLoading = () => {
-  const isLoading = useSelector(
+  const isLoading: Boolean = useSelector(
     (state: RootStateOrAny) => state.apiReducer.loading
   );
   return { isLoading };
